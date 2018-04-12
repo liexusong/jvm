20563e400c1524b9e7aa9508dd52c5b166aabbc5
xusong.lie <xusong.lie@vipshop.com>
2018-04-12 21:33:17 +0800

    add files

--- classfile/class_file.go
+++ classfile/class_file.go
@@ -53,11 +53,25 @@ func (self *ClassFile) read(reader *ClassReader) {
 func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
 	magic := reader.readUint32()
 	if magic != 0xCAFEBABE {
-		panic("java.lang.classFormatError: magic!")
+		panic("java.lang.ClassFormatError")
 	}
 }
 
 func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
+
+	self.minorVersion = reader.readUint16()
+	self.majorVersion = reader.readUint16()
+
+	switch self.majorVersion {
+	case 45:
+		return
+	case 46, 47, 48, 49, 50, 51, 52:
+		if self.minorVersion == 0 {
+			return
+		}
+	}
+
+	panic("java.lang.UnsupportedClassVersionError")
 }
 
 func (self *ClassFile) MinorVersion() uint16 {
@@ -69,15 +83,19 @@ func (self *ClassFile) MajorVersion() uint16 {
 }
 
 func (self *ClassFile) ConstantPool() ConstantPool {
+	return self.constantPool
 }
 
 func (self *ClassFile) AccessFlags() uint16 {
+	return self.accessFlags
 }
 
 func (self *ClassFile) Fields() []*MemberInfo {
+	return self.fields
 }
 
 func (self *ClassFile) Methods() []*MemberInfo {
+	return self.methods
 }
 
 func (self *ClassFile) ClassName() string {
